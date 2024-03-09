e8308db9fda82e91af7761b9a51ab01d384866f4
Aluno IFSC <ifsc@CDR-718113.cacador.ifsc.edu.br>
2024-03-08 21:12:36 -0300

    validacaologinV1

---
+++ frontend/script/login-validacao.js
@@ -0,0 +1,20 @@
+document.getElementById("login").addEventListener("submit", () => {
+    preventDefault();
+
+    let formData = new FormData(this);
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("POST","../../server/validation/validacao-login.php", true);
+    xhr.onload = () => {
+        let response = xhr.responseText;
+        console.log(response);
+
+        if(IsSuccess){
+            window.location.href = "../view/pagina-inicial.html";
+        }else{
+            alert("Falha ao Efetuar login, tente novamente")
+        }
+    }
+
+    xhr.send(formData);
+})
